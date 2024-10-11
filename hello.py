e0c9e467260058c77f9a229b601b5143b344d16d
C22363246 Jennifer Bishop <C22363246@mytudublin.ie>
2024-10-11 13:22:19 +0100

    Changed hello.py

--- hello.py
+++ hello.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 @app.route('/')
 def hello():
         return '<p>Hello, World!</p>'
-        <a href="/about">About</a>
+        <a href="/about">About page</a>
 @app.route('/about')
 def about():
         return '<p>This is an about page about the Flask web app running in a Linux VM.</p>'
