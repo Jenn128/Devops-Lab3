1050e409f1abca46e75eacb3bd0190609a37dd84
C22363246 Jennifer Bishop <C22363246@mytudublin.ie>
2024-10-11 13:08:41 +0100

    Added the about page to the hello.py file

--- hello.py
+++ hello.py
@@ -2,4 +2,11 @@ from flask import Flask
 app = Flask(__name__)
 @app.route('/')
 def hello():
-return '<p>Hello, World!</p>'
+        return '<p>Hello, World!</p>'
+        <a href="/about">About</a>
+@app.route('/about')
+def about():
+        return '<p>This is an about page about the Flask web app running in a Linux VM.</p>'
+
+if __name__ == '__main__':
+        app.run(hosts=0.0.0.0', port=8080)
